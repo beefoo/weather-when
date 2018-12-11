3aa7fe3d3a35426da29c4266287fed1dc4e91e6e
Brian Foo <brian@youaremyjoy.org>
2018-12-11 13:54:04 -0500

    add option for filling content area, add layers

--- run.py
+++ run.py
@@ -18,6 +18,7 @@ import sys
 #  python run.py -lw " 1.0,1.0" -brightness 0.5 -alpha " 0.0,255.0"
 #  python run.py -date " 2012-10-29-18" -out " +_sandy" -label " +, Hurricane Sandy"
 #  python run.py -date " 2017-08-26-18" -out " +_harvey" -label " +, Hurricane Harvey"
+#  python run.py -size fill -width 11 -height 8.5 -margin 0.5 -label "" -lw " 1.0,1.0"
 
 # input
 parser = argparse.ArgumentParser()
@@ -29,9 +30,12 @@ parser.add_argument('-rtmp', dest="REMOVE_TEMP", default=1, type=int, help="Remo
 # Image options
 parser.add_argument('-width', dest="WIDTH", default=20, type=float, help="Width of image in inches")
 parser.add_argument('-height', dest="HEIGHT", default=16, type=float, help="Height of image in inches")
-parser.add_argument('-margin', dest="MARGIN", default=3, type=float, help="Margin in inches")
+parser.add_argument('-margin', dest="MARGIN", default=1, type=float, help="Margin in inches")
 parser.add_argument('-dpi', dest="DPI", default=300, type=int, help="Dots per inch (resolution)")
 parser.add_argument('-out', dest="OUTFILE", default="", help="Output filename")
+parser.add_argument('-size', dest="RESIZE_TYPE", default="fit", help="Fit or fill")
+parser.add_argument('-offsetx', dest="OFFSETX", default=0.0, type=float, help="X offset in inches")
+parser.add_argument('-offsety', dest="OFFSETY", default=0.0, type=float, help="Y offset in inches")
 # Wind style options
 parser.add_argument('-lon', dest="LON_RANGE", default="0,360", help="Longitude range")
 parser.add_argument('-ppp', dest="POINTS_PER_PARTICLE", type=int, default=1000, help="Points per particle; higher values means longer lines")
@@ -41,9 +45,10 @@ parser.add_argument('-lw', dest="LINE_WIDTH_RANGE", default="2.0,2.0", help="Lin
 parser.add_argument('-mag', dest="MAGNITUDE_RANGE", default="0.0,12.0", help="Magnitude range")
 parser.add_argument('-alpha', dest="ALPHA_RANGE", default="0.0,255.0", help="Alpha range (0-255)")
 parser.add_argument('-blur', dest="BLUR_RADIUS", default=0.0, type=float, help="Blur radius")
-parser.add_argument('-brightness', dest="BRIGHTNESS", default=0.2, type=float, help="Brightness factor; should be 0.0 < x < 1.0; 1.0 = no change")
+parser.add_argument('-brightness', dest="BRIGHTNESS", default=0.3, type=float, help="Brightness factor; should be 0.0 < x < 1.0; 1.0 = no change")
+parser.add_argument('-layers', dest="LAYERS", default=10, type=int, help="Number of layers to composite (more = darker)")
 # Label options
-parser.add_argument('-label', dest="LABEL", default="", help="Label for image")
+parser.add_argument('-label', dest="LABEL", default="auto", help="Label for image")
 parser.add_argument('-font', dest="FONT", default="fonts/Bellefair-Regular.ttf", help="Font family")
 parser.add_argument('-fsize', dest="FONT_SIZE", default=40, type=int, help="Font size in points")
 
@@ -94,6 +99,9 @@ DPI = args.DPI
 WIDTH = args.WIDTH * DPI
 HEIGHT = args.HEIGHT * DPI
 MARGIN = args.MARGIN * DPI
+RESIZE_TYPE = args.RESIZE_TYPE
+OFFSETX = args.OFFSETX * DPI
+OFFSETY = args.OFFSETY * DPI
 
 LON_RANGE = [float(d) for d in args.LON_RANGE.strip().split(",")]
 POINTS_PER_PARTICLE = args.POINTS_PER_PARTICLE
@@ -104,13 +112,15 @@ MAGNITUDE_RANGE = tuple([float(v) for v in args.MAGNITUDE_RANGE.strip().split(",
 ALPHA_RANGE = tuple([float(v) for v in args.ALPHA_RANGE.strip().split(",")])
 BLUR_RADIUS = args.BLUR_RADIUS
 BRIGHTNESS = args.BRIGHTNESS
+LAYERS = args.LAYERS
+PARTICLES_PER_LAYER = PARTICLES / LAYERS
 SMOOTH_FACTOR = 2.0
 
 LABEL = args.LABEL.strip()
 defaultLabel = requestedDate.strftime('Wind at 10m above sea level on %B %d, %Y')
 if LABEL.startswith("+"):
     LABEL = defaultLabel + LABEL[1:]
-elif LABEL == "":
+elif LABEL == "auto":
     LABEL = defaultLabel
 FONT = args.FONT
 FONT_SIZE = args.FONT_SIZE
@@ -217,16 +227,35 @@ contentX = MARGIN
 contentY = MARGIN
 targetRatio = 1.0 * contentWidth / contentHeight
 dataRatio = 1.0 * nx / ny
-# target is wider than data, adjust width
-if targetRatio > dataRatio:
-    oldWidth = contentWidth
-    contentWidth = contentHeight * dataRatio
-    contentX += (oldWidth-contentWidth) * 0.5
-# target is narrower than data, adjust height
+cropToWidth = None
+cropToHeight = None
+
+# fill the content area
+if RESIZE_TYPE == "fill":
+    cropToWidth = contentWidth
+    cropToHeight = contentHeight
+
+    # target is wider than data, adjust height
+    if targetRatio > dataRatio:
+        contentHeight = contentWidth / dataRatio
+
+    # target is narrower than data, adjust width
+    else:
+        contentWidth = contentHeight * dataRatio
+
+# else, resize to fit
 else:
-    oldHeight = contentHeight
-    contentHeight = contentWidth / dataRatio
-    contentY += (oldHeight-contentHeight) * 0.5
+    # target is wider than data, adjust width
+    if targetRatio > dataRatio:
+        oldWidth = contentWidth
+        contentWidth = contentHeight * dataRatio
+        contentX += (oldWidth-contentWidth) * 0.5
+    # target is narrower than data, adjust height
+    else:
+        oldHeight = contentHeight
+        contentHeight = contentWidth / dataRatio
+        contentY += (oldHeight-contentHeight) * 0.5
+
 contentWidth = roundInt(contentWidth)
 contentHeight = roundInt(contentHeight)
 contentX = roundInt(contentX)
@@ -235,7 +264,19 @@ contentY = roundInt(contentY)
 print("Processing pixels...")
 scaledW = roundInt(contentWidth * SMOOTH_FACTOR)
 scaledH = roundInt(contentHeight * SMOOTH_FACTOR)
-pixels = getPixelData(windData, nx, ny, scaledW, scaledH, particleProperties, POINTS_PER_PARTICLE, VELOCITY_MULTIPLIER, MAGNITUDE_RANGE, LINE_WIDTH_RANGE, ALPHA_RANGE, BRIGHTNESS)
+if LAYERS > 1:
+    pixelLayers = np.zeros((scaledH, scaledW, LAYERS), dtype=np.uint8)
+    for l in range(LAYERS):
+        l0 = PARTICLES_PER_LAYER * l
+        l1 = min(PARTICLES_PER_LAYER * (l+1), PARTICLES)
+        pixelLayer = getPixelData(windData, nx, ny, scaledW, scaledH, particleProperties[l0:l1], POINTS_PER_PARTICLE, VELOCITY_MULTIPLIER, MAGNITUDE_RANGE, LINE_WIDTH_RANGE, ALPHA_RANGE, BRIGHTNESS)
+        pixelLayers[:,:,l] = pixelLayer
+        sys.stdout.write('\r')
+        sys.stdout.write("%s%%" % round(1.0*(l+1)/LAYERS*100,1))
+        sys.stdout.flush()
+    pixels = np.amax(pixelLayers, axis=2)
+else:
+    pixels = getPixelData(windData, nx, ny, scaledW, scaledH, particleProperties, POINTS_PER_PARTICLE, VELOCITY_MULTIPLIER, MAGNITUDE_RANGE, LINE_WIDTH_RANGE, ALPHA_RANGE, BRIGHTNESS)
 print("Building image...")
 
 # add pixels and invert
@@ -246,15 +287,21 @@ if SMOOTH_FACTOR > 0.0:
 if BLUR_RADIUS > 0:
     im = im.filter(ImageFilter.GaussianBlur(BLUR_RADIUS))
 
+# crop if necessary
+if cropToWidth and cropToHeight:
+    im = im.crop((roundInt(OFFSETX), roundInt(OFFSETY), roundInt(OFFSETX+cropToWidth), roundInt(OFFSETY+cropToHeight)))
+
 # add margin
-base = Image.new('L', (WIDTH, HEIGHT), 255)
-base.paste(im, (contentX, contentY, contentX+contentWidth, contentY+contentHeight))
+base = Image.new('L', (roundInt(WIDTH), roundInt(HEIGHT)), 255)
+cw, ch = im.size
+base.paste(im, (contentX, contentY, contentX+cw, contentY+ch))
 
 # add label
-labelMargin = roundInt(0.125 * DPI)
-fnt = ImageFont.truetype(FONT, FONT_SIZE)
-imgDraw = ImageDraw.Draw(base)
-imgDraw.text((contentX, contentY + contentHeight + labelMargin), LABEL, font=fnt, fill=0)
+if len(LABEL) > 0:
+    labelMargin = roundInt(0.125 * DPI)
+    fnt = ImageFont.truetype(FONT, FONT_SIZE)
+    imgDraw = ImageDraw.Draw(base)
+    imgDraw.text((contentX, contentY + contentHeight + labelMargin), LABEL, font=fnt, fill=0)
 
 print("Saving image...")
 base.save(OUTFILE, dpi=(DPI, DPI))
